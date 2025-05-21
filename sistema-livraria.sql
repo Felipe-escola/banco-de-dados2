67f4662126c83d1f7c4d8de7c0fcbabba322eb5b
Felipe-escola <felipe.kano@escola.pr.gov.br>
2025-05-21 08:50:32 -0300

    sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -13,3 +13,13 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(100) NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (nomeCliente, emailCliente) VALUES ('João Silva', 'joao.silva@email.com');
+INSERT INTO Clientes (nomeCliente, emailCliente) VALUES ('Maria Oliveira', 'maria.oliveira@email.com');
+INSERT INTO Clientes (nomeCliente, emailCliente) VALUES ('Carlos Santos', 'carlos.santos@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (ClienteID, NomeLivro) VALUES (1, 'O Senhor dos Anéis');
+INSERT INTO Compras (ClienteID, NomeLivro) VALUES (2, 'Harry Potter e a Pedra Filosofal');
+INSERT INTO Compras (ClienteID, NomeLivro) VALUES (3, 'A Arte da Guerra');
+SELECT * FROM Clientes;
